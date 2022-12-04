e13f20aec8b3905524533620e3021f66a7c2fa35
alswnsp411 <alswnsp411@naver.com>
2022-12-05 05:21:40 +0900

    feat: 회원가입

--- dbs_project/src/containers/RegisterPage.js
+++ dbs_project/src/containers/RegisterPage.js
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NavBar from "./NavBar";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 function RegisterPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [payment, setPayment] = useState("");
+    const [address, setAddress] = useState("");
+    const navigate = useNavigate();
+
+    const handleRegister = async(e) => {
+        e.preventDefault();
+
+        const reigster = new FormData();
+        reigster.append("email",email);
+        reigster.append("password",password);
+        reigster.append("name",name);
+        reigster.append("phoneNumber",phoneNumber);
+        reigster.append("payment",payment);
+        reigster.append("address",address);
+        try{
+            await axios(
+                {
+                    method : "POST",
+                    url : "dbs_project/register.php",
+                    data : reigster,
+                    headers : {"Content-Type" : "multipart/form-data",},
+                });
+            navigate('/login');
+            alert(`${name}님 환영합니다 ~`);
+        }catch(error){
+            console.log(error);
+        }
+    }
+
     return (
         <div className="RegisterPage">
             <NavBar/>
-            회원가입
+            <form>
+                <p>이메일</p>
+                <input type="text" placeholder="이메일"
+                       value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <p>비밀번호</p>
+                <input type="password" placeholder="비밀번호" value={password}
+                       onChange={(e) => setPassword(e.target.value)}/>
+                <p>이름</p>
+                <input type="text" placeholder="이름" value={name} onChange={(e) => setName(e.target.value)}/>
+                <p>전화번호</p>
+                <input type="text" placeholder="전화번호"
+                       value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}/>
+                <p>결제수단</p>
+                <input type="text" placeholder="결제수단"
+                       value={payment} onChange={(e) => setPayment(e.target.value)}/>
+                <p>주소</p>
+                <input type="text" placeholder="주소"
+                       value={address} onChange={(e) => setAddress(e.target.value)}/>
+                <p></p>
+                <button className="login_sign_in_button" type="submit" onClick={handleRegister}>회원가입하기</button>
+            </form>
         </div>
     );
 }
---
+++ register.php
@@ -0,0 +1,12 @@
+<?php
+$conn = mysqli_connect("192.168.3.3", "mjkim", "1234", "DBS_Project", "4567");
+
+$sql = "INSERT INTO CUSTOMER(email, password, name, phone_number, payment, address) VALUE('
+$_POST[email]','$_POST[password]','$_POST[name]','$_POST[phoneNumber]','$_POST[payment]','$_POST[address]')";
+
+if(!mysqli_query($conn, $sql)){
+    die('Error: ' .mysqli_error($conn));
+}
+echo "1 record added";
+mysqli_close($conn);
+?>
